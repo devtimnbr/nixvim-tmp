e1ee547c6dd3d944f4dff0997258669050af82e7
timneubauer <timnbr99@gmail.com>
2026-02-13 20:02:29 +0100

    stylix

--- flake.nix
+++ flake.nix
@@ -5,43 +5,31 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     nixvim.url = "github:nix-community/nixvim";
     flake-parts.url = "github:hercules-ci/flake-parts";
+    # 1. Add Stylix here so NixVim knows the 'stylix' options exist
+    stylix.url = "github:danth/stylix";
   };
 
   outputs =
-    { nixvim, flake-parts, ... }@inputs:
+    { nixvim, flake-parts, stylix, ... }@inputs:
     flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = [
-        "x86_64-linux"
-        "aarch64-linux"
-        "x86_64-darwin"
-        "aarch64-darwin"
-      ];
+      systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
 
-      perSystem =
-        { system, ... }:
+      perSystem = { system, ... }:
         let
-          nixvimLib = nixvim.lib.${system};
           nixvim' = nixvim.legacyPackages.${system};
+          
           nixvimModule = {
-            inherit system; # or alternatively, set `pkgs`
-            module = import ./config; # import the module directly
-            # You can use `extraSpecialArgs` to pass additional arguments to your module files
-            extraSpecialArgs = {
-              # inherit (inputs) foo;
+            inherit system;
+            module = {
+              imports = [ 
+                import ./config 
+                stylix.homeManagerModules.stylix
+              ];
             };
           };
-          nvim = nixvim'.makeNixvimWithModule nixvimModule;
         in
         {
-          checks = {
-            # Run `nix flake check .` to verify that your config is not broken
-            default = nixvimLib.check.mkTestDerivationFromNixvimModule nixvimModule;
-          };
-
-          packages = {
-            # Lets you run `nix run .` to start nixvim
-            default = nvim;
-          };
+          packages.default = nixvim'.makeNixvimWithModule nixvimModule;
         };
     };
 }
